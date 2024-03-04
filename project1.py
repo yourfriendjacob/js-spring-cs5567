ba3ee0e274c73970ebc2dd45913f6735365ac661
Jacob Sauther <jacobsauther@gmail.com>
2024-03-04 16:15:58 -0600

    getting ready to train

---
+++ project1.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import LabelEncoder
+from neutral import Neural
+
+# Assume 'df' is your DataFrame with categorical variables
+# Let's say 'categorical_columns' is a list of columns containing categorical data
+
+label_encoder = LabelEncoder()
+
+
+# Load your dataset
+df = pd.read_csv('data.csv')
+
+le = LabelEncoder()
+df['diagnosis'] = le.fit_transform(df['diagnosis'])
+
+df = df.drop('id',axis=1)
+df =  df.drop('Unnamed: 32',axis=1)
+
+# Calculate the correlation matrix
+correlation_matrix = df.corr()
+plt.figure(figsize=(30, 12))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
+plt.title('Correlation Heatmap')
+plt.show()
+
+# Separate the features and target
+X = df[['radius_mean','perimeter_mean','area_mean','concave points_worst','perimeter_worst','radius_worst','concave points_mean']]
+y = df['diagnosis']
+
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+y_train = y_train.to_numpy().reshape(-1,1)
+y_test = y_test.to_numpy().reshape(-1,1)
+
+epocs = 100
+learn_rate = .0003
+batch_size = 20
+val_split = .2
+verbose = 1
+
+layer_structure = [X_train.shape[1],2,2,1] #1 input for each feature, 2 neurons hl1, 2 neurons hl2, 1 output
+nn = Neural(layer_structure, epocs, learn_rate, batch_size, val_split, verbose)
+
+nn.fit(X_train, y_train)
+
+y_pred = nn.predict(X_test)
+nn.plot_learning()
+
+print("Test error: ",mean_squared_error(y_test, y_pred))
